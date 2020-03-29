2470bd847effc817c0930949a075e973612d9ceb
Kirk Min <kirk@baller.tv>
2020-03-28 18:05:19 -0700

    Updates

---
+++ precache-manifest.d1443279349fd42bb136c4d8146b8ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547d18e32d6486e8b15b2dbe11c7569b",
+    "url": "/brittanyteng.github.io/index.html"
+  },
+  {
+    "revision": "8a382ff4999989097d26",
+    "url": "/brittanyteng.github.io/static/css/main.ae8ab5e7.chunk.css"
+  },
+  {
+    "revision": "e8b6f8c7ff9c859ed935",
+    "url": "/brittanyteng.github.io/static/js/2.330c248b.chunk.js"
+  },
+  {
+    "revision": "8a382ff4999989097d26",
+    "url": "/brittanyteng.github.io/static/js/main.f9f97e6c.chunk.js"
+  },
+  {
+    "revision": "2393179aa37cb84c7bb0",
+    "url": "/brittanyteng.github.io/static/js/runtime-main.8bc8f8c2.js"
+  }
+]);
